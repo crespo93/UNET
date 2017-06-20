80b7003b3673a07785d77933326062e887f10a9c
crespo93 <jingsonc@usc.edu>
2017-06-20 15:57:47 -0700

    Client and Server scripts for the basic function

---
+++ Client.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using System.Runtime.Serialization;
+using System.IO;
+
+public class Client : MonoBehaviour
+{
+    private const int MAX_CONNECTION = 100;
+
+    private int port = 1234;
+
+    private int hostId;
+    private int webHostId;
+
+    private int reliableChannel;
+    private int unreliableChannel;
+
+    private int connectionId;
+
+    private float connectionTime;
+    private bool isConnected = false;
+    private bool isStarted = false;
+    private byte error;
+
+    private string playerName;
+
+    public void connect()
+    {
+        // player name
+        /*string pName = GameObject.Find("nameInput").GetComponent<InputField>().text;
+        if(pName == "")
+        {
+            Debug.Log("You must enter a name!");
+            return;
+        }
+
+        playerName = pName;*/
+
+        NetworkTransport.Init();
+        ConnectionConfig cc = new ConnectionConfig();
+
+        reliableChannel = cc.AddChannel(QosType.Reliable);
+        unreliableChannel = cc.AddChannel(QosType.Unreliable);
+
+        HostTopology topo = new HostTopology(cc, MAX_CONNECTION);
+        NetworkServer.Reset();
+        hostId = NetworkTransport.AddHost(topo, port, null);
+        connectionId = NetworkTransport.Connect(hostId, "10.123.210.90", 8888, 0, out error);
+        Debug.Log("connected");
+
+        connectionTime = Time.time;
+        isConnected = true;
+    }
+
+    private void Update()
+    {
+        if (!isConnected)
+            return;
+
+        int recHostId;
+        int channelId;
+        byte[] recBuffer = new byte[1024];
+        int bufferSize = 1024;
+        int dataSize;
+        byte error;
+        NetworkEventType recData = NetworkTransport.Receive(out recHostId, out connectionId, out channelId, recBuffer, bufferSize, out dataSize, out error);
+        switch (recData)
+        {
+            case NetworkEventType.Nothing:         //1
+                break;
+            case NetworkEventType.ConnectEvent:    //2
+                break;
+            case NetworkEventType.DataEvent:       //3
+                break;
+            case NetworkEventType.DisconnectEvent: //4
+                break;
+        }
+    }
+
+    public void SendSocketMessage()
+    {
+        byte error;
+        byte[] buffer = new byte[1024];
+        Vector3 position = new Vector3(1, 2, 3);
+        SerializableVector3 pos = new SerializableVector3(position.x, position.y, position.z);
+        string msg = pos.ToString();
+        Stream stream = new MemoryStream(buffer);
+        DataContractSerializer formatter = new DataContractSerializer(msg.GetType());
+        formatter.WriteObject(stream, msg);
+
+        int bufferSize = 1024;
+
+        NetworkTransport.Send(hostId, connectionId, reliableChannel, buffer, bufferSize, out error);
+    }
+
+    public struct SerializableVector3
+    {
+        /// <summary>
+        /// x component
+        /// </summary>
+        public float x;
+
+        /// <summary>
+        /// y component
+        /// </summary>
+        public float y;
+
+        /// <summary>
+        /// z component
+        /// </summary>
+        public float z;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="rX"></param>
+        /// <param name="rY"></param>
+        /// <param name="rZ"></param>
+        public SerializableVector3(float rX, float rY, float rZ)
+        {
+            x = rX;
+            y = rY;
+            z = rZ;
+        }
+
+        /// <summary>
+        /// Returns a string representation of the object
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return System.String.Format("[{0}, {1}, {2}]", x, y, z);
+        }
+
+        /// <summary>
+        /// Automatic conversion from SerializableVector3 to Vector3
+        /// </summary>
+        /// <param name="rValue"></param>
+        /// <returns></returns>
+        public static implicit operator Vector3(SerializableVector3 rValue)
+        {
+            return new Vector3(rValue.x, rValue.y, rValue.z);
+        }
+
+        /// <summary>
+        /// Automatic conversion from Vector3 to SerializableVector3
+        /// </summary>
+        /// <param name="rValue"></param>
+        /// <returns></returns>
+        public static implicit operator SerializableVector3(Vector3 rValue)
+        {
+            return new SerializableVector3(rValue.x, rValue.y, rValue.z);
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Runtime.Serialization;
+using System.IO;
+
+
+public class Server : MonoBehaviour
+{
+    private const int MAX_CONNECTION = 100;
+
+    private int port = 8888;
+
+    private int hostId;
+    private int webHostId;
+
+    private int reliableChannel;
+    private int unreliableChannel;
+
+    private bool isStarted = false;
+    private byte error;
+
+    private void Start()
+    {
+        NetworkTransport.Init();
+        ConnectionConfig cc = new ConnectionConfig();
+
+        reliableChannel = cc.AddChannel(QosType.Reliable);
+        unreliableChannel = cc.AddChannel(QosType.Unreliable);
+
+        HostTopology topo = new HostTopology(cc, MAX_CONNECTION);
+        NetworkServer.Reset();
+        hostId = NetworkTransport.AddHost(topo, port, null);
+        // webHostId = NetworkTransport.AddWebsocketHost(topo, port, null);
+
+
+        isStarted = true;
+    }
+
+    private void Update()
+    {
+        if (!isStarted)
+            return;
+
+        int recHostId;
+        int recConnectionId;
+        int recChannelId;
+        byte[] recBuffer = new byte[1024];
+        int bufferSize = 1024;
+        int dataSize;
+        byte error;
+        NetworkEventType recData = NetworkTransport.Receive(out recHostId, out recConnectionId, out recChannelId, recBuffer, bufferSize, out dataSize, out error);
+        switch (recData)
+        {
+            case NetworkEventType.Nothing:         //1
+                break;
+            case NetworkEventType.ConnectEvent:    //2
+                Debug.Log("player " + recConnectionId + "has connected");
+                break;
+            case NetworkEventType.DataEvent:       //3
+                Stream stream = new MemoryStream(recBuffer);
+                string lol = "";
+                System.Type type = lol.GetType();
+                DataContractSerializer deserializer = new DataContractSerializer(type);
+                object message = deserializer.ReadObject(stream);
+                string msg = message.ToString();
+                transform.position = getVector3(msg);
+                break;
+            case NetworkEventType.DisconnectEvent: //4
+                Debug.Log("player " + recConnectionId + "has disconnected");
+                break;
+        }
+    }
+
+    public struct SerializableVector3
+    {
+        /// <summary>
+        /// x component
+        /// </summary>
+        public float x;
+
+        /// <summary>
+        /// y component
+        /// </summary>
+        public float y;
+
+        /// <summary>
+        /// z component
+        /// </summary>
+        public float z;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="rX"></param>
+        /// <param name="rY"></param>
+        /// <param name="rZ"></param>
+        public SerializableVector3(float rX, float rY, float rZ)
+        {
+            x = rX;
+            y = rY;
+            z = rZ;
+        }
+
+        /// <summary>
+        /// Returns a string representation of the object
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return System.String.Format("[{0}, {1}, {2}]", x, y, z);
+        }
+
+        /// <summary>
+        /// Automatic conversion from SerializableVector3 to Vector3
+        /// </summary>
+        /// <param name="rValue"></param>
+        /// <returns></returns>
+        public static implicit operator Vector3(SerializableVector3 rValue)
+        {
+            return new Vector3(rValue.x, rValue.y, rValue.z);
+        }
+
+        /// <summary>
+        /// Automatic conversion from Vector3 to SerializableVector3
+        /// </summary>
+        /// <param name="rValue"></param>
+        /// <returns></returns>
+        public static implicit operator SerializableVector3(Vector3 rValue)
+        {
+            return new SerializableVector3(rValue.x, rValue.y, rValue.z);
+        }
+    }
+
+    public Vector3 getVector3(string rString)
+    {
+        string[] temp = rString.Substring(1, rString.Length - 2).Split(',');
+        float x = float.Parse(temp[0]);
+        float y = float.Parse(temp[1]);
+        float z = float.Parse(temp[2]);
+        Vector3 rValue = new Vector3(x, y, z);
+        return rValue;
+    }
+}
