b31ae6b527aef541780dfb02d9d1cac6533c51ab
crespo93 <jingsonc@usc.edu>
2017-06-30 13:33:29 -0700

    Update Server.cs

--- Server.cs
+++ Server.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
@@ -16,11 +16,18 @@ public class Server : MonoBehaviour
     private int hostId;
     private int webHostId;
 
+    private int connectionId;
+
+    private float connectionTime;
+
     private int reliableChannel;
     private int unreliableChannel;
 
     private bool isStarted = false;
     private byte error;
+
+    private GameObject move;
+    private GameObject Joint;
 
     private void Start()
     {
@@ -37,6 +44,17 @@ public class Server : MonoBehaviour
 
 
         isStarted = true;
+        //StreamWriter saveFile = new StreamWriter("output", false);
+        //saveFile.Write("");
+    }
+
+    public void connect()
+    {
+        connectionId = NetworkTransport.Connect(hostId, "10.123.210.218", 1234, 0, out error);
+        Debug.Log("connected");
+        Debug.Log(connectionId);
+
+        connectionTime = Time.time;
     }
 
     private void Update()
@@ -66,12 +84,76 @@ public class Server : MonoBehaviour
                 DataContractSerializer deserializer = new DataContractSerializer(type);
                 object message = deserializer.ReadObject(stream);
                 string msg = message.ToString();
-                transform.position = getVector3(msg);
+                char[] delimiterChars = { ':' };
+                string[] temp = msg.Split(delimiterChars);
+                string[] joint = { temp[0], temp[1], temp[2], temp[3], temp[4], temp[5]};
+                char[] Chars = { '|' };
+                Debug.Log(msg);
+                //StreamWriter saveFile = new StreamWriter("output", true);  //If the unity editor can't recognize this, change to StreamWriter saveFile = new StreamWriter(File.OpenWrite(path)); first to allow the build of client, then change it back to run 
+                StreamWriter saveFile = new StreamWriter(File.OpenWrite("output"));
+                foreach (string t in joint)
+                {
+                    string[] pos = t.Split(Chars);
+                    move = GameObject.Find(pos[0]);
+                    move.transform.position = getVector3(pos[1]);
+                    move.transform.eulerAngles = getVector3(pos[2]);
+                    float rotateAngle;
+                    switch (pos[0])
+                    {
+                        case "C3_ARM1":
+                            rotateAngle = Mathf.Round(move.transform.eulerAngles.y);
+                            break;
+                        case "C3_ARM2":
+                            rotateAngle = Mathf.Round(move.transform.eulerAngles.z);
+                            break;
+                        case "C3_ARM3":
+                            rotateAngle = Mathf.Round(move.transform.eulerAngles.z);
+                            break;
+                        case "C3_ARM4":
+                            rotateAngle = Mathf.Round(move.transform.eulerAngles.x);
+                            break;
+                        case "C3_ARM5":
+                            rotateAngle = Mathf.Round(move.transform.eulerAngles.z);
+                            break;
+                        case "C3_ARM6":
+                            rotateAngle = Mathf.Round(move.transform.eulerAngles.x);
+                            break;
+                        default:
+                            rotateAngle = 0;
+                            break;
+                    }
+                    string info = pos[0] + "|" + rotateAngle.ToString();
+                    saveFile.WriteLine(info);
+                }
+                saveFile.Dispose();
                 break;
             case NetworkEventType.DisconnectEvent: //4
                 Debug.Log("player " + recConnectionId + "has disconnected");
                 break;
         }
+    }
+
+    public void SendSocketMessage()
+    {
+        byte error;
+        string[] joints = { "C3_ARM1", "C3_ARM2", "C3_ARM3", "C3_ARM4", "C3_ARM5", "C3_ARM6" };
+        string msg = null;
+        foreach (string j in joints)
+        {
+            Joint = GameObject.Find(j);
+            Vector3 Position = Joint.transform.position;
+            Vector3 Rotation = Joint.transform.eulerAngles;
+            string name = j;
+            SerializableVector3 pos = new SerializableVector3(Position.x, Position.y, Position.z);
+            SerializableVector3 rot = new SerializableVector3(Rotation.x, Rotation.y, Rotation.z);
+            msg += name + "|" + pos.ToString() + "|" + rot.ToString() + ":";
+        }
+        byte[] buffer  = UTF8Encoding.UTF8.GetBytes(msg);
+        Stream stream = new MemoryStream(buffer);
+
+        int bufferSize = 1024;
+
+        NetworkTransport.Send(hostId, connectionId, reliableChannel, buffer, bufferSize, out error);
     }
 
     public struct SerializableVector3
@@ -144,3 +226,4 @@ public class Server : MonoBehaviour
         return rValue;
     }
 }
+
